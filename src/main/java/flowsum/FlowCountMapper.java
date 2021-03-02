0c4f18ff1525cc6fb4db5c8fc66a8ea6d893989c
ligengying <lgy961228@gmail.com>
2021-03-02 18:26:17 +0800

    添加统计

---
+++ src/main/java/flowsum/FlowCountMapper.java
@@ -0,0 +1,44 @@
+package flowsum;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * @author : ligengying
+ * @Package : flowsum
+ * @Description : TODO
+ * @Create on : 2021/3/2 15:38
+ * @email : lgy961228@gamil.com
+ **/
+public class FlowCountMapper extends Mapper<LongWritable, Text, Text, FlowBean> {
+
+    FlowBean v = new FlowBean();
+    Text k = new Text();
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        // 1 获取一行
+        String line = value.toString();
+
+        // 2 切割字段
+        String[] fields = line.split("\t");
+
+        // 3 封装对象
+        // 取出手机号码
+        String phoneNum = fields[0];
+
+        // 取出上行流量和下行流量
+        long upFlow = Long.parseLong(fields[fields.length - 1]);
+        long downFlow = Long.parseLong(fields[fields.length - 2]);
+
+        k.set(phoneNum);
+        v.set(downFlow, upFlow);
+
+        // 4 写出
+        context.write(k, v);
+    }
+}
---
+++ src/main/java/flowsum/FlowCountReducer.java
@@ -0,0 +1,34 @@
+package flowsum;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * @author : ligengying
+ * @Package : flowsum
+ * @Description : TODO
+ * @Create on : 2021/3/2 15:42
+ * @email : lgy961228@gamil.com
+ **/
+public class FlowCountReducer extends Reducer<Text, FlowBean, Text, FlowBean> {
+    @Override
+    protected void reduce(Text key, Iterable<FlowBean> values, Context context)throws IOException, InterruptedException {
+
+        long sum_upFlow = 0;
+        long sum_downFlow = 0;
+
+        // 1 遍历所用bean，将其中的上行流量，下行流量分别累加
+        for (FlowBean flowBean : values) {
+            sum_upFlow += flowBean.getUpFlow();
+            sum_downFlow += flowBean.getDownFlow();
+        }
+
+        // 2 封装对象
+        FlowBean resultBean = new FlowBean(sum_upFlow, sum_downFlow);
+
+        // 3 写出
+        context.write(key, resultBean);
+    }
+}
